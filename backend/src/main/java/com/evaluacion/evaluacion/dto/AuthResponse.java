158dc617a5c6620fb429ea0c397880a3951d3a35
manuelqh-dev <manuelqh.dev@gmail.com>
2026-09-01 12:35:57 +0200

    add LoginRequest y AuthResponse DTOs

---
+++ backend/src/main/java/com/evaluacion/evaluacion/dto/AuthResponse.java
@@ -0,0 +1,20 @@
+package com.evaluacion.evaluacion.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    private String type = "Bearer";
+    private String username;
+
+    public AuthResponse(String token, String username) {
+        this.token = token;
+        this.username = username;
+    }
+}
---
+++ backend/src/main/java/com/evaluacion/evaluacion/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.evaluacion.evaluacion.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    private String username;
+    private String password;
+
+}
